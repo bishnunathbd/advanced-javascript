badad3cc9a715c5c0023b89472d52c675a9aeeac
bishnunathbd <bishnunath774@gmail.com>
2020-08-13 16:27:56 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,25 @@
+let name;
+// console.log(name); // undefined
+
+
+function add(num1, num2) {
+   // console.log(num1 + num2);
+   return // undefined
+}
+const result = add(10, 20);
+// console.log(result);  // undefined
+
+
+function division(x, y) {
+   // console.log('y = ', y);
+}
+const result2 = division(20);
+// console.log('result2: ', result2);
+
+
+const premik = {name: 'smart dude', phone: 75757};
+// console.log(premik.address); // undefined
+
+
+let ages = [2, 5, 7];
+console.log('ages: ', ages[6]); // undefined
