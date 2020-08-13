da2bb5a1cab698868a2ffbb4be99f3a72d9ef7ee
bishnunathbd <bishnunath774@gmail.com>
2020-08-13 16:59:20 +0600

    double vs triple equal

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,17 @@
+const first = 1;
+const second = true; 
+
+// if (first == second) {
+//    console.log('condition is true');   
+// } else {
+//    console.log('condition is false');
+// }
+
+// true = 1
+// false = 0
+
+if (first === second) { // compare value & type
+   console.log('condition === is true');   
+} else {
+   console.log('condition === is false');
+}
